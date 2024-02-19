c4ae7dda7d8e4d7a5eaa8e2b3f985190ff0b7204
Gabriel Lopez <Vickyelotes69@hotmail.com>
2024-02-18 16:52:20 -0800

    Final change infinite sprint

--- FirstEverMod/Class1.cs
+++ FirstEverMod/Class1.cs
@@ -1,4 +1,7 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using FirstEverMod.Patches;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +14,29 @@ namespace FirstEverMod
     public class Nomodfirst : BaseUnityPlugin
     {
         private const string modGUID = "Vickyelotes.FirstEverMod";
+        private const string modName = "TestingMod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static Nomodfirst Instance;
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("There is no mod lmao");
+
+            harmony.PatchAll(typeof(Nomodfirst));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
     }
 }
---
+++ FirstEverMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using GameNetcodeStuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEverMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+
+        static void infiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
